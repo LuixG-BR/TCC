ce6224b22e0311fa3dfc04e91a5fba76d638bd9d
Luis Gabriel <luisgabriel19396@gmail.com>
2026-06-25 11:46:24 -0300

    adiciona cors para frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, engine
 
@@ -15,6 +16,14 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="EMPS API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(usuario.router)
