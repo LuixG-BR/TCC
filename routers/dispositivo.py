e2b47757a0c91c35f85eb4c53caa2c121101f668
LuixG-BR <luisgabriel19396@gmail.com>
2026-09-03 22:45:23 -0300

    wip: rota de edição para os dispositivos wearable

--- routers/dispositivo.py
+++ routers/dispositivo.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from database import conectar
+from core.security import verificar_token, verificar_senha
+from core.permissions import administrador_ou_medico
 
 from schemas.dispositivo import DispositivoCreate
+from models.dispositivo import Dispositivo
 
 from controllers.dispositivo import (
     criar_dispositivo,
@@ -20,7 +23,8 @@ router = APIRouter(
 @router.post("/")
 def cadastrar(
     dispositivo: DispositivoCreate,
-    db: Session = Depends(conectar)
+    db: Session = Depends(conectar),
+    usuario_token = Depends(administrador_ou_medico)
 ):
     resultado = criar_dispositivo(db, dispositivo)
 
@@ -29,8 +33,31 @@ def cadastrar(
 
 @router.get("/")
 def listar(
+    usuario_token = Depends(administrador_ou_medico),
     db: Session = Depends(conectar)
 ):
     resultado = listar_dispositivos(db)
 
     return resultado
+
+@router.put("/{id_dispositivo}")
+def editar(
+    id_dispositivo: int,
+    dados: DispositivoCreate,
+    db: Session = Depends(conectar),
+    
+    usuario_token = Depends(administrador_ou_medico)
+):
+    dispositivo = (db.query(Dispositivo)
+    .filter(
+        Dispositivo.id_dispositivo == id_dispositivo
+    ).first())
+    
+    if not dispositivo:
+        raise HTTPException(
+            status_code=404,
+            detail="Dispositivo não encontrado."
+        )
+        
+    dispositivo.numero_serie = dados.numero_serie,
+    dispositivo.id_paciente = dados.id_paciente,
